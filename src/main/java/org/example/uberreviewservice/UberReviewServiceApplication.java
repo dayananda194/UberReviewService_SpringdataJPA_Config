dbab374f130b3032c8867a906c246b9e5ced0d4b
ydayanandareddy <dayanandaredd7993163@gmail.com>
2024-07-27 23:08:44 +0530

    Added basic spring data jpa configuration to connect to databases

---
+++ src/main/java/org/example/uberreviewservice/UberReviewServiceApplication.java
@@ -0,0 +1,16 @@
+package org.example.uberreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberReviewServiceApplication.class, args);
+    }
+
+
+
+
+}
